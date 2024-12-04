22a1b1d5fa142a55733534e451b94a6a27f6c536
Majdawad88 <47667880+Majdawad88@users.noreply.github.com>
2024-12-04 15:13:08 -0500

    Add files via upload

---
+++ Stepper_Ex3.py
@@ -0,0 +1,39 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False) # Ignore warning for now stepPads = [19,13,6,5]
+stepPads = [19,13,6,5]
+GPIO.setmode(GPIO.BCM) 
+for i in range(0,4):
+	GPIO.setup(stepPads[i], GPIO.OUT)
+def motorDrive(numberSteps, Frequency, Direction): 	
+	speed = float(1/Frequency)
+	if Direction == 'R':
+		for k in range(0, numberSteps): 
+			for x in range(3,-1,-1):
+				for j in range(3,-1,-1): 
+					if x == j:
+						GPIO.output(stepPads[j],True) 
+						print("Stepp Number = ", k)
+					else:
+ 						GPIO.output(stepPads[j],False) 
+						print("Reverse")
+					time.sleep(speed) 
+	if Direction == 'F':
+		for k in range(0, numberSteps): 
+			for x in range(0,4,1):
+				for j in range(0,4,1): 
+					if x == j:
+						GPIO.output(stepPads[j],True) 
+						print("Stepp Number = ", k)
+					else:
+						GPIO.output(stepPads[j],False) 
+						print("Forward")
+
+					time.sleep(speed)
+	else:
+		print("WRONG DIRECTION ENTERED!")
+while True:
+	numSteps = int(input("Enter Number of Steps: ")) 
+	Freq = float(input("Enter Frequency: "))
+	Dir = input("Enter Direction: ") 
+	motorDrive(numSteps, Freq, Dir)
